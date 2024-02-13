8ceda5a6dd3823239db7dd0023d3d17f0cd4babd
dgomesdev <danilo.gomes@outlook.fr>
2024-02-12 21:01:28 -0400

    feat: created components styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { QuizComponent } from "../../components/quiz/quiz.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [QuizComponent]
 })
 export class HomeComponent {
 
